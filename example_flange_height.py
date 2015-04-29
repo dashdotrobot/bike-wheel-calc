d42203e6a7fcf9e150ca690f5c40f2ea88651938
dashdotrobot <mateoford@gmail.com>
2015-04-29 15:41:26 -0500

    hold rim and hub rigid and wind-up hub

--- example_flange_height.py
+++ example_flange_height.py
@@ -2,49 +2,53 @@
 
 from bikewheelfem import *
 import matplotlib.pyplot as pp
-from mpl_toolkits.mplot3d import Axes3D
-
-d_rim = 0.6
-d_hub = 0.04
-
-wheel_file = 'wheel_36x3.txt'
-
-geom = BicycleWheelGeom(wheel_file=wheel_file)
-
-r_sec = RimSection(82.0e-6,     # cross-sectional area
-                   5620.0e-12,  # I11 (torsion)
-                   1187.0e-12,  # I22 (out-of-plane bending, wobble)
-                   1124.0e-12,  # I33 (in-plane bending, squish)
-                   69.0e9,      # Young's modulus - aluminum
-                   26.0e9)      # shear modulus
-
-s_sec = SpokeSection(2.0e-3,   # diameter
-                     210e9)    # Young's modulus - steel
 
 
-flange_height = np.linspace(0.01, 0.1,10)
+# Initialize wheel geometry from wheel files
+geom = BicycleWheelGeom(wheel_file='wheel_36_x3.txt')
 
-for f in flange_height:
+# Rim section and material properties
+r_sec = RimSection(area=82.0e-6,      # cross-sectional area
+                   I11=5620.0e-12,    # area moment of inertia (twist)
+                   I22=1187.0e-12,    # area moment of inertia (wobble)
+                   I33=1124.0e-12,    # area moment of inertia (squish)
+                   young_mod=69.0e9,  # Young's modulus - aluminum
+                   shear_mod=26.0e9)  # shear modulus - aluminum
+
+# spoke section and material properties
+s_sec = SpokeSection(2.0e-3,  # spoke diameter
+                     210e9)   # Young's modulus - steel
+
+
+diam_flange = np.linspace(0.01, 0.1, 10)
+
+for d in diam_flange:
 
     # Create FEM model
-    geom.d1_hub = f
-    geom.d2_hub = f
+    geom.d1_hub = d
+    geom.d2_hub = d
     fem = BicycleWheelFEM(geom, r_sec, s_sec)
 
     # Rigid body to constrain hub nodes
-    R1 = RigidBody('hub', [0, 0, 0], fem.get_hub_nodes())
-    fem.add_rigid_body(R1)
+    r_hub = RigidBody('hub', [0, 0, 0], fem.get_hub_nodes())
+    r_rim = RigidBody('rim', [0, 0, 0], fem.get_rim_nodes())
+    fem.add_rigid_body(r_hub)
+    fem.add_rigid_body(r_rim)
 
-    fem.add_constraint(R1.node_id, range(6))  # Fix hub
-    fem.add_constraint(0, 0, 1)
+    fem.add_constraint(r_rim.node_id, range(6))   # fix rim
+    fem.add_constraint(r_hub.node_id, [2, 3, 4])  # fix hub z, roll, and yaw
+
+    fem.add_constraint(r_hub.node_id, 5, np.pi/180)  # rotate by 1 degree
 
     soln = fem.solve()
-    # print(soln.nodal_rxn)
-    # print(soln.dof_rxn)
+    print(soln.nodal_rxn)
+    print(soln.dof_rxn)
 
-    i_rxn = soln.dof_rxn.index(0)
+    i_rxn = 9  # index of reaction torque on rim
 
-    pp.plot(f, soln.nodal_rxn[i_rxn],'ro')
+    pp.plot(d*100, soln.nodal_rxn[i_rxn], 'ro')
 
+pp.xlabel('flange diameter [cm]')
+pp.ylabel('wind-up stiffness [N-m / degree]')
 
 pp.show()
