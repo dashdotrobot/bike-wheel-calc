3c313ecb37a5f12db7d8081c477b6bd612362c83
dashdotrobot <mateoford@gmail.com>
2015-04-29 14:33:49 -0500

    simplified and commented code

--- example_stand_on_spokes.py
+++ example_stand_on_spokes.py
@@ -2,35 +2,34 @@
 
 from bikewheelfem import *
 import matplotlib.pyplot as pp
-from mpl_toolkits.mplot3d import Axes3D
 
-d_rim = 0.6
-d_hub = 0.04
 
-file_36x3 = 'wheel_36_x3.txt'
-file_crow = 'wheel_36_crowsfoot.txt'
+# Initialize wheel geometry from wheel files
+geom_36x3 = BicycleWheelGeom(wheel_file='wheel_36_x3.txt')
+geom_crow = BicycleWheelGeom(wheel_file='wheel_36_crowsfoot.txt')
 
-geom_36x3 = BicycleWheelGeom(wheel_file=file_36x3)
-geom_crow = BicycleWheelGeom(wheel_file=file_crow)
+# Rim section and material properties
+r_sec = RimSection(area=82.0e-6,      # cross-sectional area
+                   I11=5620.0e-12,    # area moment of inertia (twist)
+                   I22=1187.0e-12,    # area moment of inertia (wobble)
+                   I33=1124.0e-12,    # area moment of inertia (squish)
+                   young_mod=69.0e9,  # Young's modulus - aluminum
+                   shear_mod=26.0e9)  # shear modulus - aluminum
 
-r_sec = RimSection(82.0e-6,     # cross-sectional area
-                   5620.0e-12,  # I11 (torsion)
-                   1187.0e-12,  # I22 (out-of-plane bending, wobble)
-                   1124.0e-12,  # I33 (in-plane bending, squish)
-                   69.0e9,      # Young's modulus - aluminum
-                   26.0e9)      # shear modulus
-
-s_sec = SpokeSection(2.0e-3,   # diameter
-                     210e9)    # Young's modulus - steel
+# spoke section and material properties
+s_sec = SpokeSection(2.0e-3,  # spoke diameter
+                     210e9)   # Young's modulus - steel
 
 
 # --- 36-spoke cross-3 lacing --------------------------------
 fem = BicycleWheelFEM(geom_36x3, r_sec, s_sec)
 
+# Create a rigid body for the hub nodes and constrain it
 R1 = RigidBody('hub', [0, 0, 0], fem.get_hub_nodes())
 fem.add_rigid_body(R1)
+fem.add_constraint(R1.node_id, range(6))
 
-fem.add_constraint(R1.node_id, range(6))  # Fix hub
+# Add an upward force, distributed over bottom 3 nodes
 fem.add_force(0,1,500)
 fem.add_force(1,1,250)
 fem.add_force(35,1,250)
@@ -59,8 +58,8 @@ fem = BicycleWheelFEM(geom_crow, r_sec, s_sec)
 
 R1 = RigidBody('hub', [0, 0, 0], fem.get_hub_nodes())
 fem.add_rigid_body(R1)
+fem.add_constraint(R1.node_id, range(6))
 
-fem.add_constraint(R1.node_id, range(6))  # Fix hub
 fem.add_force(0,1,500)
 fem.add_force(1,1,250)
 fem.add_force(35,1,250)
