685e38995fa509b9e7ba2ed7fd89b3cb220951a8
Matt Ford <mateoford@gmail.com>
2016-07-22 13:59:42 -0500

    rect2pol(p)

--- bikewheelcalc/helpers.py
+++ bikewheelcalc/helpers.py
@@ -25,3 +25,12 @@ def pol2rect(p):
     'Convert a point from polar coordinates to Cartesian coordinates.'
 
     return np.array([p[0]*np.sin(p[1]), -p[0]*np.cos(p[1]), p[2]])
+
+
+def rect2pol(p):
+    'Convert a point from Cartesian coordinates to polar coordinates.'
+
+    r = np.sqrt(p[0]**2 + p[1]**2)
+    theta = np.arctan2(p[0], -p[1])
+
+    return np.array([r, theta, p[2]])
