c1b1c69ec720efa8a8a0726b6cf9a75c50622c0d
Matt Ford <mateoford@gmail.com>
2016-07-24 14:17:47 -0500

    Update examples to new BicycleWheel code

--- example_stand_on_spokes.py
+++ example_stand_on_spokes.py
@@ -1,27 +1,24 @@
-#!/usr/bin/env python
-
 import bikewheelcalc as bc
 import matplotlib.pyplot as pp
 
 
-# Initialize wheel geometry from wheel files
-geom_36x3 = bc.WheelGeometry(wheel_file='wheel_36_x3.txt')
+# Create an example wheel
+wheel = bc.BicycleWheel()
+wheel.hub = wheel.Hub(diam1=0.04, width1=0.03)
+wheel.rim = wheel.Rim.general(radius=0.3,
+                              area=82.0e-6,
+                              I11=5620e-12,
+                              I22=1187e-12,
+                              I33=1124e-12,
+                              Iw=0.0,
+                              young_mod=69.0e9,
+                              shear_mod=26.0e9)
 
-# Rim section and material properties
-r_sec = bc.RimSection(area=82.0e-6,      # cross-sectional area
-                      I11=5620.0e-12,    # area moment of inertia (twist)
-                      I22=1187.0e-12,    # area moment of inertia (wobble)
-                      I33=1124.0e-12,    # area moment of inertia (squish)
-                      young_mod=69.0e9,  # Young's modulus - aluminum
-                      shear_mod=26.0e9)  # shear modulus - aluminum
-
-# spoke section and material properties
-s_sec = bc.SpokeSection(2.0e-3,  # spoke diameter
-                        210e9)   # Young's modulus - steel
-
+wheel.lace_cross(n_spokes=36, n_cross=3, diameter=2.0e-3,
+                 young_mod=210e9, offset=0.0)
 
 # Create finite-element model from wheel
-fem = bc.BicycleWheelFEM(geom_36x3, r_sec, s_sec)
+fem = bc.BicycleWheelFEM(wheel, verbose=True)
 
 # Create a rigid body for the hub nodes and constrain it
 R1 = bc.RigidBody('hub', [0, 0, 0], fem.get_hub_nodes())
@@ -37,11 +34,9 @@ soln = fem.solve(pretension=1000)
 
 # Draw deformed wheel
 f1 = pp.figure(1)
-f_def = soln.plot_deformed_wheel(scale_rad=0.1)
+f_def = soln.plot_deformed_wheel(rel_scale=0.1)
 pp.axis('off')
-pp.savefig('def_36x3.png')
 
-# Plot spoke tension
 f2 = pp.figure(2)
 soln.plot_spoke_tension(fig=f2)
 f2.gca().set_yticklabels([])
