d48bb73708bb62a26f94b65602c603810217ec39
Matt Ford <mateoford@gmail.com>
2018-10-24 10:37:54 -0500

    Create very basic setup.py script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='bikewheelcalc',
+      version='0.1-alpha',
+      description='',
+      url='https://github.com/dashdotrobot/bike-wheel-calc.git',
+      author='Matthew Ford',
+      author_email='mateoford@gmail.com',
+      license='MIT',
+      packages=['bikewheelcalc'])
