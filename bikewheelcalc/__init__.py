f8fec80c062a7e167e3975329ebed18474f3c4a7
Matt Ford <mateoford@gmail.com>
2018-10-24 09:28:43 -0500

    Move FEM code to fem/ submodule

--- bikewheelcalc/__init__.py
+++ bikewheelcalc/__init__.py
@@ -1,6 +1,2 @@
 from .bicycle_wheel import *
 from .mode_matrix import ModeMatrix
-from .bikewheelfem import BicycleWheelFEM
-from .femsolution import FEMSolution
-from .rigidbody import RigidBody
-from .helpers import *
