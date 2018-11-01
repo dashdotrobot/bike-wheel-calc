6a051ab2aa3294f14763e73e0ab49d95522c4e2e
Matt Ford <mateoford@gmail.com>
2018-11-01 17:36:39 -0500

    apply_tension() now accepts T_avg, T_left, or T_right

--- bikewheelcalc/bicycle_wheel.py
+++ bikewheelcalc/bicycle_wheel.py
@@ -276,19 +276,38 @@ class BicycleWheel:
 
         # Assume that there are only two tensions in the wheel: left and right
         # and that spokes alternate left, right, left, right...
-        s_0 = self.spokes[0]
-        s_1 = self.spokes[1]
+        s_l = self.spokes[0]
+        s_r = self.spokes[1]
 
-        T_0 = 2 * T_avg * np.abs(s_1.n[0]) /\
-            (np.abs(s_0.n[0]*s_1.n[1]) + np.abs(s_1.n[0]*s_0.n[1]))
-        T_1 = 2 * T_avg * np.abs(s_0.n[0]) /\
-            (np.abs(s_0.n[0]*s_1.n[1]) + np.abs(s_1.n[0]*s_0.n[1]))
+        if T_avg is not None:  # Specify average radial tension
+            T_l = 2 * T_avg * np.abs(s_r.n[0]) /\
+                (np.abs(s_l.n[0]*s_r.n[1]) + np.abs(s_r.n[0]*s_l.n[1]))
+            T_r = 2 * T_avg * np.abs(s_l.n[0]) /\
+                (np.abs(s_l.n[0]*s_r.n[1]) + np.abs(s_r.n[0]*s_l.n[1]))
 
+            for i in range(0, len(self.spokes), 2):
+                self.spokes[i].tension = T_l
+
+            for i in range(1, len(self.spokes), 2):
+                self.spokes[i].tension = T_r
+
+        elif T_right is not None:  # Specify right-side tension
+            T_r = T_right
+            T_l = np.abs(s_r.n[0]/s_l.n[0]) * T_right
+
+        elif T_left is not None:  # Specify left-side tension
+            T_l = T_left
+            T_r = np.abs(s_l.n[0]/s_r.n[0]) * T_left
+
+        else:
+            raise TypeError('Must specify one of the following arguments: T_avg, T_left, or T_right.')
+
+        # Apply tensions
         for i in range(0, len(self.spokes), 2):
-            self.spokes[i].tension = T_0
+            self.spokes[i].tension = T_l
 
         for i in range(1, len(self.spokes), 2):
-            self.spokes[i].tension = T_1
+            self.spokes[i].tension = T_r
 
     def calc_kbar(self, tension=True):
         'Calculate smeared-spoke stiffness matrix'
--- tests/test_bicycle_wheel.py
+++ tests/test_bicycle_wheel.py
@@ -134,12 +134,62 @@ def test_calc_kbar_offset_zero_eig(std_ncross, offset):
     assert np.allclose(np.min(w), 0.)
 
 @pytest.mark.parametrize('n_cross', [0, 1, 2, 3])
-def test_apply_tension(std_ncross, n_cross):
+def test_apply_tension_T_avg(std_ncross, n_cross):
     'Test that the apply_tension() method gives the correct T_avg'
 
     w = std_ncross(n_cross)
-    w.apply_tension(100.0)
+
+    # Make hub asymmetric
+    w.hub.width_left = 0.03
+    w.hub.width_right = 0.02
+    w.lace_cross(n_spokes=36, n_cross=n_cross, diameter=1.8e-3, young_mod=210e9, offset=0.)
+
+    w.apply_tension(T_avg=100.0)
 
     Tavg = np.sum([s.n[1]*s.tension for s in w.spokes]) / len(w.spokes)
 
     assert np.allclose(Tavg, 100.0)
+    assert np.allclose(np.sum([s.n[0]*s.tension for s in w.spokes[::2]]),
+                       -np.sum([s.n[0]*s.tension for s in w.spokes[1::2]]))
+
+@pytest.mark.parametrize('n_cross', [0, 1, 2, 3])
+def test_apply_tension_right(std_ncross, n_cross):
+    'Test that the apply_tension() method works with T_right'
+
+    w = std_ncross(n_cross)
+
+    # Make hub asymmetric
+    w.hub.width_left = 0.03
+    w.hub.width_right = 0.02
+    w.lace_cross(n_spokes=36, n_cross=n_cross, diameter=1.8e-3, young_mod=210e9, offset=0.)
+
+    w.apply_tension(T_right=100.0)
+
+    assert np.allclose([s.tension for s in w.spokes[1::2]], 100.0)
+    assert np.allclose(np.sum([s.n[0]*s.tension for s in w.spokes[::2]]),
+                       -np.sum([s.n[0]*s.tension for s in w.spokes[1::2]]))
+
+@pytest.mark.parametrize('n_cross', [0, 1, 2, 3])
+def test_apply_tension_left(std_ncross, n_cross):
+    'Test that the apply_tension() method works with T_left'
+
+    w = std_ncross(n_cross)
+
+    # Make hub asymmetric
+    w.hub.width_left = 0.03
+    w.hub.width_right = 0.02
+    w.lace_cross(n_spokes=36, n_cross=n_cross, diameter=1.8e-3, young_mod=210e9, offset=0.)
+
+    w.apply_tension(T_left=100.0)
+
+    assert np.allclose([s.tension for s in w.spokes[0::2]], 100.0)
+    assert np.allclose(np.sum([s.n[0]*s.tension for s in w.spokes[::2]]),
+                       -np.sum([s.n[0]*s.tension for s in w.spokes[1::2]]))
+
+def test_apply_tension_none(std_ncross):
+    'Test that the apply_tension() method throws error with no arguments'
+
+    w = std_ncross(1)
+
+    with pytest.raises(TypeError):
+        w.apply_tension()
