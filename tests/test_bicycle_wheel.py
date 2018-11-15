6453c6b38b5e7fc3fec8dd540b85f7122072aaac
Matt Ford <mateoford@gmail.com>
2018-11-15 15:10:06 -0600

    Test that I_wheel is less than theoretical max.

--- tests/test_bicycle_wheel.py
+++ tests/test_bicycle_wheel.py
@@ -322,3 +322,18 @@ def test_I_spokes_only():
         I_wheel = w.calc_rot_inertia()
 
     assert np.allclose(I_wheel, 36.*I_spk)
+
+def test_I_wheel_less_than_max():
+    'Check that rotational inertia is less than theoretical maximum'
+
+    w = BicycleWheel()
+    w.hub = Hub(diameter=0.050, width=0.05)
+    w.rim = Rim(radius=0.3, area=100e-6,
+                I11=25./26e9, I22=200./69e9, I33=100./69e9, Iw=0.0,
+                young_mod=69e9, shear_mod=26e9, density=1.0)
+
+    w.lace_radial(n_spokes=36, diameter=1.8e-3, young_mod=210e9, offset=0., density=1.0)
+
+    I_wheel = w.calc_rot_inertia()
+
+    assert I_wheel < w.calc_mass()*w.rim.radius**2
