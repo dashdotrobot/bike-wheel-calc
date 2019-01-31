6fc380c3576307d825d24fdffbbcbc192a720300
Matt Ford <mateoford@gmail.com>
2019-01-31 17:04:39 -0600

    Update setup.py with version number and description

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='bikewheelcalc',
-      version='0.1-alpha',
-      description='',
+      version='0.9.0',
+      description='Stress analysis of bicycle wheels implemented in Python using NumPy',
       url='https://github.com/dashdotrobot/bike-wheel-calc.git',
       author='Matthew Ford',
       author_email='mateoford@gmail.com',
