9cdb1dafa12682b70e10ae21a81b8aca8687f408
Matt Ford <mateoford@gmail.com>
2020-02-09 12:24:07 -0500

    Draft empty unit tests for calculating internal forces

---
+++ tests/test_internal_force.py
@@ -0,0 +1,26 @@
+import pytest
+import warnings
+import numpy as np
+from bikewheelcalc import BicycleWheel, Rim, Hub
+
+# -----------------------------------------------------------------------------
+# Internal force tests
+# -----------------------------------------------------------------------------
+
+def test_normal_force():
+	assert False
+
+def test_shear_rad():
+	assert False
+
+def test_shear_lat():
+	assert False
+
+def test_moment_rad():
+	assert False
+
+def test_moment_lat():
+	assert False
+
+def test_moment_twist():
+	assert False
